561ab822bf5e0a4e1b88754df84129217633c31e
Kacper Sulek <kacpersulek15022004@gmail.com>
2024-12-13 14:47:29 +0100

    .

---
+++ basic/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!\n";
+  cout << "Have a good day!\n";
+  cout << 2 * 5 << "\n";
+  cout << 8*2/3;
+
+  int myNum;
+  myNum = 15;
+  cout << myNum;
+
+  return 0; 
+}
